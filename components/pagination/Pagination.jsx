34435dc8738c86f7911d3472266acad7f24b3c4e
Yoru Yeh <orchardsky31@gmail.com>
2023-12-07 15:39:09 +0800

    style: rewrite pagination style

--- components/pagination/Pagination.jsx
+++ components/pagination/Pagination.jsx
@@ -2,102 +2,78 @@
 
 import { useState } from 'react';
 
-// const currentPage = 2;
-const totalPage = 20;
-
 export default function Pagination() {
-  const [currentPage, setCurrentPage] = useState(2);
+  const [currentPageData, setCurrentPageData] = useState({
+    currentPage: 2,
+    totalPage: 10,
+  });
 
-  const pager = () => {
-    const pages = [];
-    const centerPage = currentPage;
-
-    if (currentPage < 3) {
-      for (let i = 1; i <= 5; i++) {
-        pages.push(i);
-      }
-    } else if (currentPage > totalPage - 3) {
-      for (let i = totalPage - 4; i <= totalPage; i++) {
-        pages.push(i);
-      }
-    } else {
-      for (let i = currentPage - 2; i <= centerPage + 2; i++) {
-        pages.push(i);
-      }
-    }
-
-    return pages;
-  };
-
-  const generatePages = () => {
-    const pagesArr = pager();
-
-    return pagesArr.map((page) => (
-      <button
-        key={`page_${page}`}
-        className={`flex h-10 w-10 items-center justify-center rounded-md ${
-          currentPage === page ? 'bg-primary-yellow-500' : 'border'
-        }`}
-        onClick={() => setCurrentPage(page)}
-      >
-        {page}
-      </button>
-    ));
+  const onClick = (n) => {
+    setCurrentPageData((prev) => ({
+      ...prev,
+      currentPage: prev.currentPage + n,
+    }));
   };
 
   return (
-    <div className='flex h-fit w-full items-center justify-center gap-3 py-2'>
-      <button
-        disabled={currentPage === 1}
-        className={`flex h-10 w-10 items-center justify-center rounded-md border ${
-          currentPage === 1 ? 'text-grey-500' : ''
-        }`}
-        onClick={() => setCurrentPage((prev) => prev - 1)}
-      >
-        ＜
+    <div className='mt-1 flex h-fit w-full items-center justify-center gap-3 px-10 py-3 md:justify-end'>
+      <button className='text-title-m h-10 w-[58px] rounded-md border-2 border-grey-700 bg-grey-800 font-inter text-grey-100'>
+        {currentPageData.currentPage}
       </button>
-      {currentPage >= 4 && (
-        <>
-          <button
-            className={`flex h-10 w-10 items-center justify-center rounded-md ${
-              currentPage === 1 ? 'bg-primary-yellow-500' : 'border'
-            }`}
-            onClick={() => setCurrentPage(1)}
-          >
-            1
-          </button>
-          <button className='flex h-10 w-10 items-center justify-center rounded-md border'>
-            ...
-          </button>
-        </>
-      )}
+      <p className='text-title-m font-inter text-grey-400'>
+        of {currentPageData.totalPage === 0 ? 1 : currentPageData.totalPage}
+      </p>
 
-      {generatePages()}
-
-      {currentPage <= 17 && (
-        <>
-          <button className='flex h-10 w-10 items-center justify-center rounded-md border'>
-            ...
-          </button>
-          <button
-            className={`flex h-10 w-10 items-center justify-center rounded-md ${
-              currentPage === totalPage ? 'bg-primary-yellow-500' : 'border'
-            }`}
-            onClick={() => setCurrentPage(totalPage)}
-          >
-            {totalPage}
-          </button>
-        </>
-      )}
-
+      {/* 上一頁 */}
       <button
-        disabled={currentPage === totalPage}
-        className={`flex h-10 w-10 items-center justify-center rounded-md border ${
-          currentPage === totalPage ? 'text-grey-500' : ''
-        }`}
-        onClick={() => setCurrentPage((prev) => prev + 1)}
+        className='flex h-10 w-10 items-center justify-center rounded-md bg-grey-600'
+        disabled={currentPageData.currentPage === 1}
+        onClick={() => onClick(-1)}
       >
-        ＞
+        <svg
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+          className='rotate-180'
+        >
+          <path
+            d='M9 18L15 12L9 6'
+            stroke={currentPageData.currentPage === 1 ? '#323F52' : '#EFF2F5'}
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+        </svg>
+      </button>
+
+      {/* 下一頁 */}
+      <button
+        className='flex h-10 w-10 items-center justify-center rounded-md bg-grey-600'
+        disabled={currentPageData.currentPage === currentPageData.totalPage}
+        onClick={() => onClick(1)}
+      >
+        <svg
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path
+            d='M9 18L15 12L9 6'
+            stroke={
+              currentPageData.currentPage === currentPageData.totalPage ||
+              currentPageData.totalCount === 0
+                ? '#323F52'
+                : '#EFF2F5'
+            }
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          />
+        </svg>
       </button>
     </div>
   );
