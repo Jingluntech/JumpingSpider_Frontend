0e4a4b2ec2d400908566fb19068546777d2b2f7d
Yoru Yeh <orchardsky31@gmail.com>
2023-12-07 16:20:01 +0800

    style: rewrite record table style

--- components/tables/RecordTable.jsx
+++ components/tables/RecordTable.jsx
@@ -3,38 +3,45 @@ export default function RecordTable() {
     <div className='h-fit w-full overflow-hidden rounded-lg'>
       <table className='w-full'>
         <thead className='bg-grey-800'>
-          <tr className='h-16 border-b border-grey-500'>
-            <th className='w-1/5 px-6 py-3 text-left text-sm'>訂閱時間</th>
-            <th className='w-1/5 px-6 py-3 text-left text-sm'>訂閱方案</th>
-            <th className='w-1/5 px-6 py-3 text-left text-sm'>到期日</th>
-            <th className='w-1/5 px-6 py-3 text-left text-sm'>狀態</th>
-            <th className='w-1/5 px-6 py-3 text-left text-sm'>操作</th>
+          <tr className='h-11 border-b border-grey-700'>
+            <th className='w-1/5 px-6 py-3 text-left text-sm font-medium text-grey-300'>
+              訂閱時間
+            </th>
+            <th className='w-1/5 px-6 py-3 text-left text-sm font-medium text-grey-300'>
+              訂閱到期日
+            </th>
+            <th className='px-6 py-3 text-left text-sm font-medium text-grey-300'>
+              訂閱內容
+            </th>
+            <th className='w-1/5 px-6 py-3 text-left text-sm font-medium text-grey-300'>
+              金額
+            </th>
           </tr>
         </thead>
         <tbody>
           <tr className='h-20 border-b border-grey-500'>
-            <td className='w-1/5 px-6 py-3 text-left text-sm'>2023/06/12</td>
             <td className='w-1/5 px-6 py-3 text-left text-sm'>
-              企業版VPN（12個月）
+              2023-06-01 21:00
             </td>
-            <td className='w-1/5 px-6 py-3 text-left text-sm'>2024/06/12</td>
-            <td className='w-1/5 px-6 py-3 text-left text-sm'>訂閱中</td>
-            <td className='w-1/5 px-6 py-3 text-left text-sm text-primary-yellow-500'>
-              訂閱資訊
+            <td className='w-1/5 px-6 py-3 text-left text-sm'>
+              2023-06-01 21:00
             </td>
+            <td className='px-6 py-3 text-left text-sm'>
+              Jumping Spider VPN Subscription - 12 Months
+            </td>
+            <td className='w-1/5 px-6 py-3 text-left text-sm'>UTC 321.00</td>
           </tr>
           <tr className='h-20 border-b border-grey-500'>
-            <td className='w-1/5 px-6 py-3 text-left text-sm'>2023/06/12</td>
             <td className='w-1/5 px-6 py-3 text-left text-sm'>
-              企業版VPN（12個月）
+              2023-06-01 21:00
             </td>
-            <td className='w-1/5 px-6 py-3 text-left text-sm'>2024/06/12</td>
-            <td className='w-1/5 px-6 py-3 text-left text-sm text-secondary-red-500'>
-              已過期
+            <td className='w-1/5 px-6 py-3 text-left text-sm'>
+              2023-06-01 21:00
             </td>
-            <td className='w-1/5 px-6 py-3 text-left text-sm text-primary-yellow-500'>
-              訂閱資訊
+            <td className='px-6 py-3 text-left text-sm'>
+              Jumping Spider VPN訂閱 - 12個月
             </td>
+            <td className='w-1/5 px-6 py-3 text-left text-sm'>UTC 321.00</td>
           </tr>
         </tbody>
       </table>
