2a125cae8144ccdc2abb8d12cb0b1ac45f4a3a5d
Yoru Yeh <orchardsky31@gmail.com>
2023-12-07 13:31:27 +0800

    style: add wallet icons and fix styles

--- components/modals/ConnectWallet.jsx
+++ components/modals/ConnectWallet.jsx
@@ -12,6 +12,7 @@ import {
 
 import ethersClient from '@/utils/eth/ethersClient';
 import { use } from 'i18next';
+import Image from 'next/image';
 
 export default function ConnectWallet({ onClick }) {
   const { open } = useWeb3Modal();
@@ -80,34 +81,82 @@ export default function ConnectWallet({ onClick }) {
 
   return (
     <div className='absolute left-1/2 top-1/2 z-20 flex h-fit w-full min-w-[359px] max-w-[516px] -translate-x-1/2 -translate-y-1/2 flex-col gap-[14px] rounded-md bg-grey-900 p-6'>
-      <h4 className='text-h4 font-bold'>Connect Wallet</h4>
+      <h4 className='text-2xl font-bold'>連接錢包</h4>
       <button
         className='flex h-[74px] w-full items-center justify-between rounded-md bg-grey-700 px-6 py-3'
         onClick={() => walletConnectHandler('metamask')}
       >
-        <div className='flex items-center gap-4'>Metamask</div>
+        <div className='flex items-center gap-4'>
+          <Image
+            src='/wallet/wallet_metamask.svg'
+            alt='meta-mask-icon'
+            width={50}
+            height={50}
+          />
+          <h5 className='text-h5 font-bold'>Metamask</h5>
+        </div>
+        <Image
+          src='/wallet/wallet_icon_arrow.svg'
+          alt='arrow-icon'
+          width={24}
+          height={24}
+        />
       </button>
 
       <button
         className='flex h-[74px] w-full items-center justify-between rounded-md bg-grey-700 px-6 py-3'
         onClick={() => walletConnectHandler('trustWallet')}
       >
-        <div className='flex items-center gap-4'>Trust Wallet</div>
+        <div className='flex items-center gap-4'>
+          <Image
+            src='/wallet/wallet_trustwallet.svg'
+            alt='meta-mask-icon'
+            width={50}
+            height={50}
+          />
+          <h5 className='text-h5 font-bold'>Trust Wallet</h5>
+        </div>
+        <Image
+          src='/wallet/wallet_icon_arrow.svg'
+          alt='arrow-icon'
+          width={24}
+          height={24}
+        />
       </button>
 
       <button
         className='flex h-[74px] w-full items-center justify-between rounded-md bg-grey-700 px-6 py-3'
         onClick={() => walletConnectHandler('walletConnect')}
       >
-        <div className='flex items-center gap-4'>WalletConnect</div>
+        <div className='flex items-center gap-4'>
+          <Image
+            src='/wallet/wallet_walletconnect.svg'
+            alt='meta-mask-icon'
+            width={50}
+            height={50}
+          />
+          <h5 className='text-h5 font-bold'>WalletConnect</h5>
+        </div>
+        <Image
+          src='/wallet/wallet_icon_arrow.svg'
+          alt='arrow-icon'
+          width={24}
+          height={24}
+        />
       </button>
 
       <button
         className='flex h-14 w-full items-center justify-center gap-2 rounded-md bg-grey-100'
         onClick={onClick}
       >
+        <Image
+          src='/wallet/wallet_button_back.svg'
+          alt='back-icon'
+          width={24}
+          height={24}
+        />
         <h5 className='text-h5 border-gray-900 font-bold text-grey-800'>
-          Back
+          返回
         </h5>
       </button>
     </div>
--- components/modals/ModalBackground.jsx
+++ components/modals/ModalBackground.jsx
@@ -1,7 +1,7 @@
 export default function ModalBackground({ onClick }) {
   return (
     <div
-      className='fixed inset-0 z-10 bg-grey-300 opacity-80'
+      className='fixed inset-0 z-10 bg-grey-600 opacity-80'
       onClick={onClick}
     ></div>
   );
