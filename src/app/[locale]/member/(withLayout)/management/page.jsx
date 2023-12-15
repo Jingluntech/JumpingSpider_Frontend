f98d61199ce8676b3a0d849fa55fbe43e8c287d6
Yoru Yeh <orchardsky31@gmail.com>
2023-12-15 17:07:29 +0800

    feat: write device table

--- src/app/[locale]/member/(withLayout)/management/page.jsx
+++ src/app/[locale]/member/(withLayout)/management/page.jsx
@@ -1,5 +1,5 @@
 import Pagination from '@/src/app/components/pagination/Pagination';
-import RecordTable from '@/src/app/components/tables/RecordTable';
+import DeviceTable from '@/src/app/components/tables/DeviceTable';
 import pick from 'lodash.pick';
 import {
   NextIntlClientProvider,
@@ -13,8 +13,8 @@ export default function MemberManagementPage() {
   return (
     <div className='mx-auto mb-[27px] flex h-fit w-full min-w-[350px] max-w-[1216px] flex-col gap-6 px-4 py-[56px]'>
       <h1 className='mb-4 text-[46px] font-bold'>{t('management')}</h1>
-      <NextIntlClientProvider messages={pick(messages, 'recordTable')}>
-        <RecordTable />
+      <NextIntlClientProvider messages={pick(messages, 'devicePage')}>
+        <DeviceTable />
       </NextIntlClientProvider>
       <Pagination />
     </div>
---
+++ src/app/components/tables/DeviceTable.jsx
@@ -0,0 +1,104 @@
+import { useTranslations } from 'next-intl';
+
+export default function DeviceTable() {
+  const t = useTranslations('devicePage');
+
+  return (
+    <div className='h-fit w-full overflow-hidden rounded-lg'>
+      <table className='w-full lg:hidden'>
+        <tbody className='border-b border-grey-600'>
+          <tr className='min-h-11 h-11 text-left'>
+            <th className='w-1/2 p-3 text-sm font-medium text-grey-300'>#</th>
+            <td className='w-1/2 p-3'>1</td>
+          </tr>
+          <tr className='min-h-11 h-11 text-left'>
+            <th className='w-1/2 p-3 text-sm font-medium text-grey-300'>
+              {t('name')}
+            </th>
+            <td className='w-1/2 p-3'>11CB00413</td>
+          </tr>
+          <tr className='min-h-11 h-11 text-left'>
+            <th className='w-1/2 p-3 text-sm font-medium text-grey-300'>
+              {t('time')}
+            </th>
+            <td className='w-1/2 p-3'>2023-06-01 21:00</td>
+          </tr>
+          <tr className='min-h-11 h-11 text-left'>
+            <th className='w-1/2 p-3 text-sm font-medium text-grey-300'>
+              {t('operate')}
+            </th>
+            <td className='w-1/2 cursor-pointer p-3 text-primary-yellow-500 hover:text-grey-100'>
+              {t('delete')}
+            </td>
+          </tr>
+        </tbody>
+        <tbody className='border-b border-grey-600'>
+          <tr className='min-h-11 h-11 text-left'>
+            <th className='w-1/2 p-3 text-sm font-medium text-grey-300'>#</th>
+            <td className='w-1/2 p-3'>1</td>
+          </tr>
+          <tr className='min-h-11 h-11 text-left'>
+            <th className='w-1/2 p-3 text-sm font-medium text-grey-300'>
+              {t('name')}
+            </th>
+            <td className='w-1/2 p-3'>11CB00413</td>
+          </tr>
+          <tr className='min-h-11 h-11 text-left'>
+            <th className='w-1/2 p-3 text-sm font-medium text-grey-300'>
+              {t('time')}
+            </th>
+            <td className='w-1/2 p-3'>2023-06-01 21:00</td>
+          </tr>
+          <tr className='min-h-11 h-11 text-left'>
+            <th className='w-1/2 p-3 text-sm font-medium text-grey-300'>
+              {t('operate')}
+            </th>
+            <td className='w-1/2 cursor-pointer p-3 text-primary-yellow-500 hover:text-grey-100'>
+              {t('delete')}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table className='hidden w-full lg:table'>
+        <thead className='bg-grey-800'>
+          <tr className='h-11 border-b border-grey-700'>
+            <th className='w-1/12 px-6 py-3 text-left text-sm font-medium text-grey-300'>
+              #
+            </th>
+            <th className='w-4/12 px-6 py-3 text-left text-sm font-medium text-grey-300'>
+              {t('name')}
+            </th>
+            <th className='w-5/12 px-6 py-3 text-left text-sm font-medium text-grey-300'>
+              {t('time')}
+            </th>
+            <th className='w-2/12 px-6 py-3 text-left text-sm font-medium text-grey-300'>
+              {t('operate')}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className='h-20 border-b border-grey-500'>
+            <td className='w-1/12 px-6 py-3 text-left text-sm'>1</td>
+            <td className='w-4/12 px-6 py-3 text-left text-sm'>11CB00413</td>
+            <td className='w-5/12 px-6 py-3 text-left text-sm'>
+              2023-06-01 21:00
+            </td>
+            <td className='w-2/12 cursor-pointer px-6 py-3 text-left text-sm text-primary-yellow-500 hover:text-grey-100'>
+              {t('delete')}
+            </td>
+          </tr>
+          <tr className='h-20 border-b border-grey-500'>
+            <td className='w-1/12 px-6 py-3 text-left text-sm'>1</td>
+            <td className='w-4/12 px-6 py-3 text-left text-sm'>11CB00413</td>
+            <td className='w-5/12 px-6 py-3 text-left text-sm'>
+              2023-06-01 21:00
+            </td>
+            <td className='w-2/12 cursor-pointer px-6 py-3 text-left text-sm text-primary-yellow-500 hover:text-grey-100'>
+              {t('delete')}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
