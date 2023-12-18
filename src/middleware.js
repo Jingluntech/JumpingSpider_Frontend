68d0074712d4c2f56ada486387aaedf3c42586a6
Yoru Yeh <orchardsky31@gmail.com>
2023-12-18 10:53:23 +0800

    feat: rewrite middleware to redirect the unauthorized request

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,30 @@
 import createMiddleware from 'next-intl/middleware';
 import { locales, localePrefix } from './navigation';
+import { redirect } from 'next/navigation';
 
-export default createMiddleware({
-  defaultLocale: 'en',
-  localePrefix,
-  locales,
-});
+export default async function middleware(request) {
+  const isLogin = request.cookies.get('Token');
+
+  if (request.url.includes('member') && !isLogin) {
+    const url = new URL(request.url);
+    const redirectTo = url.origin + '/';
+    return new Response(null, {
+      status: 302,
+      headers: {
+        Location: redirectTo,
+      },
+    });
+  }
+
+  const handleI18nRouting = createMiddleware({
+    defaultLocale: 'en',
+    localePrefix,
+    locales,
+  });
+  const response = handleI18nRouting(request);
+
+  return response;
+}
 
 export const config = {
   // Match only internationalized pathnames
