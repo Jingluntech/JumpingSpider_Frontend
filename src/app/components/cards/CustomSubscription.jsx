49288ec7bd719fc1fcb5f81112247a8bb2d6bb96
Yoru Yeh <orchardsky31@gmail.com>
2024-01-09 11:00:54 +0800

    style: fix subscription card

--- src/app/components/cards/CustomSubscription.jsx
+++ src/app/components/cards/CustomSubscription.jsx
@@ -6,19 +6,19 @@ export default function CustomSubscription() {
 
   return (
     <div className='flex h-fit w-full max-w-[602px] flex-col gap-5 rounded-md border-[3px] border-grey-500 bg-grey-800 px-10 py-11 lg:flex-1 lg:gap-8'>
-      <div>
+      <div className='flex h-1/2 flex-col'>
         <p className='mb-1 text-sm text-primary-yellow-500'>{t('business')}</p>
-        <h1 className='mb-3 text-[28px] font-medium lg:whitespace-nowrap'>
+        <h1 className='text-[28px] font-medium lg:whitespace-nowrap'>
           {t('customized')}
         </h1>
-        <p className='mb-3 flex text-base text-grey-300 lg:h-[88px]'>
+        <p className='mt-3 flex text-base text-grey-300 lg:h-fit'>
           {t('planDescription')}
         </p>
       </div>
 
       <hr className='border-t-[2px] border-grey-600' />
 
-      <div className='flex h-fit w-full flex-col gap-3 rounded-md bg-grey-700 px-5 py-6 lg:flex-row-reverse lg:justify-between lg:px-6 lg:py-8'>
+      <div className='flex h-1/2 w-full flex-col gap-3 rounded-md bg-grey-700 px-5 py-6 lg:flex-row-reverse lg:justify-between lg:px-6 lg:py-8'>
         <div className='relative h-[148px] w-[158px] lg:flex-shrink-0'>
           <Image
             src='/home/Business_vpn.svg'
--- src/app/components/cards/Subscription.jsx
+++ src/app/components/cards/Subscription.jsx
@@ -51,8 +51,8 @@ export default function Subscription() {
   }, []);
 
   return (
-    <div className='flex h-fit w-full max-w-[602px] flex-col gap-5 rounded-md border-[3px] border-primary-blue-500 bg-grey-800 px-10 py-11 lg:flex-1'>
-      <div className='flex flex-col gap-3 lg:mb-8 lg:flex-row-reverse lg:justify-between'>
+    <div className='flex h-fit w-full max-w-[602px] flex-col gap-5 rounded-md border-[3px] border-primary-blue-500 bg-grey-800 px-10 py-11 lg:flex-1 lg:gap-8'>
+      <div className='flex h-1/2 flex-col gap-3 lg:flex-row-reverse lg:justify-between'>
         <div className='relative h-[156px] w-[174px]'>
           <Image
             src='/home/monthly_vpn.svg'
@@ -65,7 +65,7 @@ export default function Subscription() {
           <h1 className='text-[28px] font-medium lg:whitespace-nowrap'>
             {t('plan')}
           </h1>
-          <p className='mt-3 flex h-16 items-center font-inter text-base text-grey-300'>
+          <p className='mt-6 flex h-fit items-center font-inter text-base text-grey-300'>
             USDT
             <span className='mx-2 font-inter text-[46px] font-bold text-grey-100'>
               30.00
@@ -77,113 +77,115 @@ export default function Subscription() {
 
       <hr className='border-t-[2px] border-grey-600' />
 
-      <ul className='flex flex-col gap-4 lg:mb-5'>
-        <li className='flex gap-3 text-base font-medium text-grey-300'>
-          <Image
-            src='/alert/alerts_icon_success.svg'
-            alt='success icon'
-            width={16}
-            height={16}
-          />
-          {t('featureOne')}
-        </li>
-        <li className='flex gap-3 text-base font-medium text-grey-300'>
-          <Image
-            src='/alert/alerts_icon_success.svg'
-            alt='success icon'
-            width={16}
-            height={16}
-          />
-          {t('featureTwo')}
-        </li>
-        <li className='flex gap-3 text-base font-medium text-grey-300'>
-          <Image
-            src='/alert/alerts_icon_success.svg'
-            alt='success icon'
-            width={16}
-            height={16}
-          />
-          {t('featureThree')}
-        </li>
-        <li className='flex gap-3 text-base font-medium text-grey-300'>
-          <Image
-            src='/alert/alerts_icon_success.svg'
-            alt='success icon'
-            width={16}
-            height={16}
-          />
-          {t('featureFour')}
-        </li>
-      </ul>
-      {!pathname.includes('price') ? (
-        <Link href='/price'>
-          <button className='h-11 w-full rounded-md bg-primary-blue-500 hover:bg-grey-100 hover:text-grey-800'>
-            {t('more')}
-          </button>
-        </Link>
-      ) : (
-        <div className='flex w-full flex-wrap items-center gap-5 lg:flex-nowrap'>
-          <div className='text-sm text-grey-300'>{t('months')}</div>
-          <div className='flex h-11 w-[180px]'>
-            <button
-              className='flex h-full flex-1 items-center rounded-l-md border border-grey-600 bg-grey-700 p-3'
-              disabled={inputValue === 1}
-              onClick={() => setInputValue((prev) => prev - 1)}
-            >
-              <svg
-                width='20'
-                height='20'
-                viewBox='0 0 20 20'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path
-                  d='M4 10H16'
-                  stroke={inputValue === 1 ? '#949EAE' : '#EFF2F5'}
-                  strokeWidth='2'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                />
-              </svg>
+      <div className='flex h-1/2 flex-col'>
+        <ul className='flex flex-col gap-4 lg:mb-5'>
+          <li className='flex gap-3 text-base font-medium text-grey-300'>
+            <Image
+              src='/alert/alerts_icon_success.svg'
+              alt='success icon'
+              width={16}
+              height={16}
+            />
+            {t('featureOne')}
+          </li>
+          <li className='flex gap-3 text-base font-medium text-grey-300'>
+            <Image
+              src='/alert/alerts_icon_success.svg'
+              alt='success icon'
+              width={16}
+              height={16}
+            />
+            {t('featureTwo')}
+          </li>
+          <li className='flex gap-3 text-base font-medium text-grey-300'>
+            <Image
+              src='/alert/alerts_icon_success.svg'
+              alt='success icon'
+              width={16}
+              height={16}
+            />
+            {t('featureThree')}
+          </li>
+          <li className='flex gap-3 text-base font-medium text-grey-300'>
+            <Image
+              src='/alert/alerts_icon_success.svg'
+              alt='success icon'
+              width={16}
+              height={16}
+            />
+            {t('featureFour')}
+          </li>
+        </ul>
+        {!pathname.includes('price') ? (
+          <Link href='/price'>
+            <button className='h-11 w-full rounded-md bg-primary-blue-500 hover:bg-grey-100 hover:text-grey-800'>
+              {t('more')}
             </button>
-            <div className='flex-2 border-y border-grey-600 bg-grey-900 align-middle text-grey-100'>
-              <input
-                type='number'
-                disabled
-                className='h-full w-full bg-transparent text-center text-grey-100'
-                value={inputValue}
-              />
+          </Link>
+        ) : (
+          <div className='flex w-full flex-wrap items-center gap-5 lg:flex-nowrap'>
+            <div className='text-sm text-grey-300'>{t('months')}</div>
+            <div className='flex h-11 w-[180px]'>
+              <button
+                className='flex h-full flex-1 items-center rounded-l-md border border-grey-600 bg-grey-700 p-3'
+                disabled={inputValue === 1}
+                onClick={() => setInputValue((prev) => prev - 1)}
+              >
+                <svg
+                  width='20'
+                  height='20'
+                  viewBox='0 0 20 20'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    d='M4 10H16'
+                    stroke={inputValue === 1 ? '#949EAE' : '#EFF2F5'}
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                  />
+                </svg>
+              </button>
+              <div className='flex-2 border-y border-grey-600 bg-grey-900 align-middle text-grey-100'>
+                <input
+                  type='number'
+                  disabled
+                  className='h-full w-full bg-transparent text-center text-grey-100'
+                  value={inputValue}
+                />
+              </div>
+              <button
+                className='flex h-full flex-1 items-center rounded-r-md border border-grey-600 bg-grey-700 p-3'
+                disabled={inputValue === 12}
+                onClick={() => setInputValue((prev) => prev + 1)}
+              >
+                <svg
+                  width='20'
+                  height='20'
+                  viewBox='0 0 20 20'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                >
+                  <path
+                    d='M10 4V16M4 10H16'
+                    stroke={inputValue === 12 ? '#949EAE' : '#EFF2F5'}
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                  />
+                </svg>
+              </button>
             </div>
             <button
-              className='flex h-full flex-1 items-center rounded-r-md border border-grey-600 bg-grey-700 p-3'
-              disabled={inputValue === 12}
-              onClick={() => setInputValue((prev) => prev + 1)}
+              className='h-11 w-[164px] rounded-md bg-primary-blue-500 hover:bg-grey-100 hover:text-grey-800'
+              onClick={() => handlePayClick()}
             >
-              <svg
-                width='20'
-                height='20'
-                viewBox='0 0 20 20'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path
-                  d='M10 4V16M4 10H16'
-                  stroke={inputValue === 12 ? '#949EAE' : '#EFF2F5'}
-                  strokeWidth='2'
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                />
-              </svg>
+              {t('pay')}
             </button>
           </div>
-          <button
-            className='h-11 w-[164px] rounded-md bg-primary-blue-500 hover:bg-grey-100 hover:text-grey-800'
-            onClick={() => handlePayClick()}
-          >
-            {t('pay')}
-          </button>
-        </div>
-      )}
+        )}
+      </div>
 
       {checkOutOpen && (
         <>
